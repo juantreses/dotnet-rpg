5d4f9ae90475bf518a60693dcd7e8b633189863e
Jan Dries <jandries@live.be>
2024-02-28 09:05:45 +0100

    Add CharacterEndpoint

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
+global using dotnet_rpg.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,6 +18,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 var summaries = new[]
 {
